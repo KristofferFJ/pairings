353d5b5be4efa4b5a161c50bfc6d63a5f7d81952
kfj <kfj@netcompany.com>
2022-04-24 09:08:31 +0200

    setup project

---
+++ src/main/java/io/github/kristofferfj/pairings/PairingsApplication.java
@@ -0,0 +1,13 @@
+package io.github.kristofferfj.pairings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairingsApplication.class, args);
+	}
+
+}
